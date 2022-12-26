76e483367cf16b721ea697de1e42116f455c53a9
mariopetkovskii <petkovski_mario@yahoo.com>
2022-12-26 19:17:04 +0100

    added backend and frontend applications

---
+++ backend/artificalyzer/src/test/java/com/artificalyzer/ArtificalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artificalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtificalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
